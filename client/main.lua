409ab99833f33f167bb54b47d82645a0b28d3c68
GMW Scripts <79488475+GermanWarthog@users.noreply.github.com>
2023-11-08 11:02:38 +0100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -21,7 +21,7 @@ local filename = function()
     local str = debug.getinfo(2, "S").source:sub(2)
     return str:match("^.*/(.*).lua$") or str
 end
-print("^6[CLIENT - DEBUG] ^0: "..filename()..".lua gestartet");
+print("^6[CLIENT - DEBUG] ^0: "..filename()..".lua started");
 
 -- ════════════════════════════════════════════════════════════════════════════════════ --
 -- Code
